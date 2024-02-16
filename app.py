2688ececc95acd0cca4eeca9e52ee19d2488222f
bingus-bongo <100236235+bingus-bongo@users.noreply.github.com>
2024-02-16 17:59:24 -0600

    please wokr

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import string
 import random
 from datetime import datetime
-from flask import Flask, g
+from flask import Flask, g, request, jsonify
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -64,13 +65,134 @@ def page_not_found(e):
 
 # TODO: Create the API
 
-# @app.route('/api/signup')
-# def login():
-#   ...
+@app.route('/api/signup')
+def signup():
+  user = new_user()
+  api_key = user[3]
+  username = user[1]
+  return jsonify({
+      'api-key': api_key, 
+      'username': username
+    })
 
+@app.route('/api/login', methods=['POST'])
+def login():
+  username = request.headers.get('username')
+  password = request.headers.get('password')
 
-# @app.route('/api/login')
-# def login():
-#   ... 
+  user = query_db('SELECT * FROM users WHERE name = ?', [username])
+  user_dict = dict(user[0])
 
-# ... etc
+  if user_dict['password'] == password:
+      print('succeeded')
+      return jsonify({
+          'status': 'success',
+          'api-key': user_dict['api_key'],
+          'username': user_dict['name'],
+          })
+  else:
+      return jsonify({
+          'status': 'failed',
+          })
+
+@app.route('/api/rooms/<int:room_id>')
+def get_messages(room_id):
+    messages = query_db('SELECT * FROM messages WHERE room_id = ?', [room_id])
+    message_dicts = []
+    if messages:
+        for message in messages:
+            columns = message.keys()
+            message_dict = {col: message[col] for col in columns}
+            message_dicts.append(message_dict)
+        users = query_db('SELECT * FROM users')
+        user_dicts = []
+        for user in users:
+            columns = user.keys()
+            user_dict = {col: user[col] for col in columns}
+            user_dicts.append(user_dict)
+            for message_dict in message_dicts:
+                if message_dict['user_id'] == user_dict['id']:
+                    message_dict['name'] = user_dict['name']
+        return jsonify({'status': 'success', 'message_dict': message_dicts})
+    else:
+        return jsonify({'status': 'failed'})
+
+@app.route('/api/roomname/<int:room_id>')
+def get_room_info(room_id):
+    room = query_db('SELECT * FROM rooms WHERE id = ?', [room_id])
+    room_dict = dict(room)
+    return room_dict
+
+@app.route('/api/post/message', methods=['POST'])
+def post_message():
+    room_id = request.headers.get('room-id')
+    message = request.headers.get('message')
+    api_key = request.headers.get('api-key')
+    user = query_db('SELECT * FROM users WHERE api_key = ?', [api_key])
+    user_dict = dict(user[0])
+    id = user_dict['id']
+    query_db('INSERT INTO messages (user_id, room_id, body) VALUES (?, ?, ?)', [id, room_id, message])
+    return jsonify({
+        'message': 'success',
+        'code': 200
+    }), 200
+
+@app.route('/api/update/room/name', methods=['POST'])
+def update_room():
+    new_room_name = request.headers.get('new-room-name')
+    api_key = request.headers.get('api-key')
+    room_id = request.headers.get('room-id')
+    query_db('UPDATE rooms SET name = ? WHERE id = ?', [new_room_name, room_id])
+    return jsonify({
+        'message': 'success',
+        'code': 200
+    }), 200
+
+@app.route('/api/update/username', methods=['POST'])
+def update_username():
+    print("updaing username")
+    api_key = request.headers.get('api-key')
+    new_username = request.headers.get('new-username')
+    query_db('UPDATE users SET name = ? WHERE api_key = ?', [new_username, api_key])
+    return jsonify({
+        'message': 'success',
+        'code': 200
+    }), 200
+
+@app.route('/api/update/password', methods=['POST'])
+def update_password():
+    api_key = request.headers.get('api-key')
+    if api_key:
+        new_password = request.headers.get('new-password')
+        query_db('UPDATE users SET password = ? WHERE api_key = ?', [new_password, api_key])
+        return jsonify({
+            'message': 'success',
+            'code': 200
+        }), 200
+    else:
+        return jsonify({
+            'error_message': "API Authentication failed",
+            'code': 401
+        }), 401
+
+@app.route('/api/create/room', methods=['POST'])
+def create_room():
+    api_key = request.headers.get('api-key')
+    roomname = request.headers.get('roomname')
+    if api_key:
+        query_db('INSERT INTO rooms (name) VALUES (?)', [roomname])
+        return jsonify({
+            'message': 'success',
+            'code': 200
+        }), 200
+
+@app.route('/api/get/rooms')
+def get_rooms():
+    api_key = request.headers.get('api-key')
+    rooms = query_db('SELECT * FROM rooms')
+    room_dicts = []
+    for room in rooms:
+        columns = room.keys()
+        room_dict = {col: room[col] for col in columns}
+        room_dicts.append(room_dict)
+    return jsonify(room_dicts)
--- static/script.js
+++ static/script.js
@@ -4,6 +4,9 @@ const PROFILE = document.querySelector(".profile");
 const LOGIN = document.querySelector(".login");
 const ROOM = document.querySelector(".room");
 
+// For checking messsages
+let message_interval;
+
 // Custom validation on the password reset fields
 const passwordField = document.querySelector(".profile input[name=password]");
 const repeatPasswordField = document.querySelector(".profile input[name=repeatPassword]");
@@ -37,6 +40,282 @@ repeatPasswordField.addEventListener("input", checkPasswordRepeat);
 //          login successfully, send them to their original destination
 //        - Hide all other pages
 
+function gotoSplash() {
+  stopMessagePolling();
+  const splash_url = '/';
+  history.pushState({}, '', splash_url);
+  SPLASH.classList.remove('hide');
+  PROFILE.classList.add('hide');
+  LOGIN.classList.add('hide');
+  ROOM.classList.add('hide');
+}
+
+function gotoProfile() {
+  stopMessagePolling();
+  const profile_url = '/profile';
+  history.pushState({}, '', profile_url);
+  SPLASH.classList.add('hide');
+  PROFILE.classList.remove('hide');
+  LOGIN.classList.add('hide');
+  ROOM.classList.add('hide');
+}
+
+function gotoLogin() {
+  stopMessagePolling();
+  const login_url = '/';
+  history.pushState({}, '', login_url);
+  SPLASH.classList.add('hide');
+  PROFILE.classList.add('hide');
+  LOGIN.classList.remove('hide');
+  ROOM.classList.add('hide');
+}
+
+function gotoRoom() {
+  startMessagePolling();
+  const room_id = sessionStorage.getItem('room_id');
+  const room_url = '/room/' + room_id;
+  history.pushState({}, '', room_url);
+  SPLASH.classList.add('hide');
+  PROFILE.classList.add('hide');
+  LOGIN.classList.add('hide');
+  ROOM.classList.remove('hide');
+}
+
+// Configures what splash looks like based on if we are logged in, gets and sets up links to rooms
+async function configureSplash() {
+  let api_key = localStorage.getItem('api-key');
+  const username = localStorage.getItem('username');
+  const logged_in = Boolean(api_key);
+
+  splashLoggedOut = document.querySelector('.splash .loggedOut'); // Show only to logged out
+  splashLoggedIn = document.querySelector('.splash .loggedIn');   // Show only to logged in users
+  RoomCreateButton = document.querySelector('.create');           // Shows create room when logged in
+  SignupButton = document.querySelector('.signup');               // Shows signup to logged out
+
+  if (logged_in) {
+    splashLoggedOut.classList.add('hide');
+    splashLoggedIn.classList.remove('hide');
+    RoomCreateButton.classList.remove('hide');
+    SignupButton.classList.add('hide');
+
+    // Configure welcome back message if logged in
+    splashLoggedInUsername = SPLASH.querySelector('.splash .username');
+    splashLoggedInUsername.innerHTML = 'Welcome back, ' + username + '!';
+
+    // Clear roomlist
+    roomList = document.querySelector('.roomList');
+    roomList.innerHTML = '';
+
+    // Get roomlist and create anchor list
+    const url = '/api/get/rooms';
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    const rooms = await response.json();
+    rooms.forEach(room => {
+      let newRoom = document.createElement('a');
+      newRoom.innerHTML = room['id'] + ': <strong>' + room['name'] + '</strong>';
+      roomList.appendChild(newRoom);
+      
+      if (logged_in) {
+          newRoom.addEventListener('click', () => {
+              sessionStorage.setItem('room_id', room['id']);
+              configureRoom(room['id']);
+              gotoRoom();
+          });
+      }
+    })
+
+    noRooms = document.querySelector('.noRooms');
+    if (roomList.length == 0) {
+      noRooms.classList.remove('hide');
+    } else {
+      noRooms.classList.add('hide');
+    }
+  } else {
+    splashLoggedOut.classList.remove('hide');
+    splashLoggedIn.classList.add('hide');
+    RoomCreateButton.classList.add('hide');
+    SignupButton.classList.remove('hide');
+  }
+}
+
+// Sets up what profile should look like
+function configureProfile() {
+  let api_key = localStorage.getItem('api-key');
+  const username = localStorage.getItem('username');
+  const logged_in = Boolean(api_key);
+  
+  if (logged_in) {
+    profileUsername = document.querySelector('.profile .username');
+    profileUsername.innerHTML = username;
+    profileUsernameInput = document.querySelector('input[name="username"]');
+    profileUsernameInput.value = username;
+  }
+}
+
+// Sets up what login page should look like, failed message configured later
+function configureLogin() {
+  failedLogin = document.querySelector('.failed .message');
+  failedLoginButton = document.querySelector('.failed button');
+  failedLogin.classList.add('hide');
+  failedLoginButton.classList.add('hide');
+}
+
+// Sets up what room should look like
+async function configureRoom(room_id) {
+  let api_key = localStorage.getItem('api-key');
+  const username = localStorage.getItem('username');
+  const logged_in = Boolean(api_key);
+  
+  if (logged_in) {
+    sessionStorage.setItem('room_id', room_id);
+    const roomUsername = document.querySelector('.room .username');
+    roomUsername.innerHTML = username;
+
+    // Get room info and setup headers stuff
+    const room_url = `/api/roomname/${room_id}`;
+    const room_response = await fetch(room_url, {
+      method: 'GET',
+    })
+    const room_info = await room_response.json();
+    displayName = document.querySelector('.displayRoomName strong');          // Sets the roomname at the top
+    displayName.innerHTML = room_info[room_id];
+    inviteLink = document.querySelector('.inviteLink');
+    inviteLink.innerHTML = '/rooms/' + room_id;
+
+    displayRoomName = document.querySelector('.roomDetail .displayRoomName'); // Show by default, hide when edit is clicked
+    displayRoomName.classList.remove('hide');
+    editRoomName = document.querySelector('.roomDetail .editRoomName');       // Hide by default, show when edit is clicked
+    editRoomName.classList.add('hide');
+
+    // Clear sample messages
+    const chatbox = document.body.querySelector('.messages');
+    chatbox.innerHTML = '';
+
+    // Get and fill chatbox
+    const chat_url = `/api/rooms/${room_id}`;
+    let chat_response = await fetch(chat_url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': api_key,
+      },
+    })
+    const messages = await chat_response.json();
+    console.log(messages);
+    if (messages['status'] == 'success'){
+      console.log('hi');
+      sessionStorage.setItem('curr_message', messages['message_dict'].length);
+      for (message of messages['message_dict']) {
+        new_message = document.createElement('message');
+        new_author = document.createElement('author');
+        new_author.innerHTML = message['name'];
+        new_content = document.createElement('content');
+        new_content.innerHTML = message['body'];
+        new_message.appendChild(new_author);
+        new_message.appendChild(new_content);
+        chatbox.appendChild(new_message);
+      }
+    }
+
+    // ROOM - Setup edit roomname buttons
+    editIcon = document.querySelector('.roomDetail .material-symbols-outlined');
+    editIcon.addEventListener('click', () => {
+      displayRoomName.classList.add('hide');
+      editRoomName.classList.remove('hide');
+    })
+
+    editRoomNameButton = document.querySelector('.editRoomName button');
+    editRoomNameButton.addEventListener('click', async () => {
+      let new_room_name = document.querySelector('.editRoomName input').value;
+      if (Boolean(new_room_name)) {
+        const update_room_name_url = '/api/update/room/name';
+        await fetch(update_room_name_url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': api_key,
+            'new-room-name': new_room_name,
+            'room-id': room_id
+          }
+        })
+        displayName.innerHTML = new_room_name;
+        displayRoomName.classList.remove('hide');
+        editRoomName.classList.add('hide');
+      }
+    })
+
+    // ROOM - Listen for posting message, send to database
+    submitbutton = document.querySelector('.chat button');
+    submitbutton.addEventListener('click', async () => {
+      let new_message = document.querySelector('.chat textarea').value;
+      if (Boolean(new_message)) {
+        const post_url = '/api/post/message';
+        await fetch(post_url, {
+          method: 'POST',
+          headers: {
+            'api-key': api_key,
+            'room-id': room_id,
+            'message': new_message,
+          }
+        })
+      }
+    })
+  }
+}
+
+function signout() {
+  localStorage.removeItem('api-key');
+  localStorage.removeItem('username');
+}
+
+function configurePages() {
+  configureSplash();
+  configureProfile();
+  // configureRoom();
+  configureLogin();
+}
+
+function loadPageFromPath() {
+  let api_key = localStorage.getItem('api-key');
+  const logged_in = Boolean(api_key);
+  path = window.location.pathname;
+
+  // Display splash regardless of credentials
+  if (path == '/') {
+    gotoSplash();
+  } 
+  // Redirect from login to splash if logged in
+  if (path == '/login' && logged_in) {
+    gotoSplash();
+  }
+  // Show room if logged in
+  if (path.startsWith('/room/') && logged_in) {
+    const url_room_id = path.substring('/room/'.length);
+    const room_id = parseInt(url_room_id);
+    configureRoom(room_id);
+    gotoRoom();
+  } 
+  // Show profile if logged in
+  if (path == '/profile' && logged_in) {
+    gotoProfile();
+  }
+  // Show login page if not logged in
+  if (!logged_in) {
+    if (path == '/login') {
+      gotoLogin();                
+    } else if (path.startsWith('/room/')) {
+      sessionStorage.setItem('saved_path', path);
+      gotoLogin();
+    } else if (path == '/profile') {
+      sessionStorage.setItem('saved_path', path);
+      gotoLogin();
+    }
+  }
+}
+
 // TODO:  When displaying a page, update the DOM to show the appropriate content for any element
 //        that currently contains a {{ }} placeholder. You do not have to parse variable names out
 //        of the curly  braces—they are for illustration only. You can just replace the contents
@@ -48,8 +327,237 @@ repeatPasswordField.addEventListener("input", checkPasswordRepeat);
 //        - When the user goes to a new "page" ("/", "/login", "/profile", or "/room"), push it to
 //          History
 
+// General - logged in - Clicking welcome back button goes to profile page
+profileButtons = document.querySelectorAll('.welcomeBack');
+profileButtons.forEach(profileButton => {
+  profileButton.addEventListener('click', () => {
+    gotoProfile();
+  });
+});
+
+// SPLASH - Not logged in - Login button goes to login page
+loginPageButton = document.querySelector('.loggedOut a');
+loginPageButton.addEventListener('click', () => {
+  configureLogin();
+  gotoLogin();
+});
+
+// SPLASH - Not logged in - Signup button signs up, logs in, then reconfigures the pages and returns to splash
+signupButton = document.querySelector('.signup');
+signupButton.addEventListener('click', async () => {
+  const url = '/api/signup';
+  const response = await fetch(url, {
+    method: 'GET'
+  });
+
+  const user_info = await response.json();
+  const api_key = user_info['api-key'];
+  const username = user_info['username'];
+
+  localStorage.setItem('api-key', api_key);
+  localStorage.setItem('username', username);
+
+  configurePages();
+  gotoSplash();
+});
+
+// SPLASH - logged in - Create a room button
+splashCreateRoomButton = document.querySelector('button.create');
+splashCreateRoomButton.addEventListener('click', async () => {
+  let api_key = localStorage.getItem('api-key');
+  const find_room_num_url = '/api/get/rooms';
+  const response = await fetch(find_room_num_url, {
+    method: 'GET',
+    headers: {
+      'api-key': api_key
+    }
+  });
+  const rooms = await response.json();
+  let roomLen = rooms.length + 1;
+
+  const create_room_url = '/api/create/room'
+  await fetch(create_room_url, {
+    method: 'POST',
+    headers: {
+      'api-key': api_key,
+      'roomname': 'Room ' + roomLen
+     }
+  })
+  configureRoom(roomLen + 1);
+  gotoRoom();
+});
+
+// LOGIN - login button for logging in
+const loginButton = LOGIN.querySelector('.login button');
+const usernameInput = LOGIN.querySelector('input[name="username"]');
+const passwordInput = LOGIN.querySelector('input[name="password"]');
+loginButton.addEventListener('click', async () => {
+  username = usernameInput.value;
+  password = passwordInput.value;
+  if (Boolean(username) && Boolean(password)) {
+    const login_url = '/api/login';
+    const login_response = await fetch(login_url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'username': username,
+        'password': password
+      }
+    })
+  
+    const login_info = await login_response.json();
+    if (login_info['status'] == 'success') {
+      localStorage.setItem('api-key', login_info['api-key']);
+      localStorage.setItem('username', login_info['username']);
+      const saved_path = sessionStorage.getItem('saved_path');
+      if (Boolean(saved_path)) {
+        history.pushState(null, '', saved_path);
+        configurePages();
+        loadPageFromPath();
+      } else {
+        configurePages();
+        gotoSplash();
+      }
+    } else {
+      failedLogin = document.querySelector('.failed .message');
+      failedLoginButton = document.querySelector('.failed button');
+      failedLogin.classList.remove('hide');
+      failedLoginButton.classList.remove('hide');
+    }
+  } else {
+    failedLogin = document.querySelector('.failed .message');
+    failedLoginButton = document.querySelector('.failed button');
+    failedLogin.classList.remove('hide');
+    failedLoginButton.classList.remove('hide');
+  }
+})
+
+// LOGIN - Create account button, same function as one click signup
+loginPageCreateButton = document.querySelector('.failed button');
+loginPageCreateButton.addEventListener('click', async () => {
+  const signup_url = '/api/signup';
+  const response = await fetch(signup_url, {
+    method: 'GET'
+  })
+
+  const user_info = await response.json();
+  const api_key = user_info['api-key'];
+  const username = user_info['username'];
+
+  localStorage.setItem('api-key', api_key);
+  localStorage.setItem('username', username);
+
+  configurePages();
+  gotoSplash();
+});
+
+// PROFILE - Update username
+updateUsernameButton = PROFILE.querySelector('.updateUsernameButton');
+updateUsernameButton.addEventListener('click', async () => {
+  let usernameInput = PROFILE.querySelector('input[name="username"]').value;
+  if (Boolean(usernameInput)) {
+    const update_username_url = '/api/update/username';
+    let api_key = localStorage.getItem('api-key');
+    let usernameInput = PROFILE.querySelector('input[name="username"]').value;
+    let response = await fetch(update_username_url, {
+      method: 'POST',
+      headers: {
+        'api-key': api_key,
+        'new-username': usernameInput,
+      }
+    })
+    if (response.ok) {
+      localStorage.setItem('username', usernameInput);
+    }
+  }
+})
+
+// PROFILE - Update password
+updatePasswordButton = PROFILE.querySelector('.updateUsernameButton');
+updatePasswordButton.addEventListener('click', async () => {
+  let passwordInput = passwordField.value;
+  if (Boolean(passwordInput)) {
+    let api_key = localStorage.getItem('api-key');
+    const update_password_url = '/api/update/password';
+    await fetch(update_password_url, {
+      method: 'POST',
+      headers: {
+        'api-key': api_key,
+        'new-password': passwordInput,
+      }
+    })
+  }
+})
+
+// PROFILE - Signout button
+logoutButton = PROFILE.querySelector('.logout');
+logoutButton.addEventListener('click', () => {
+  signout();
+  configureSplash();
+  gotoSplash();
+});
+
+// PROFILE - Coolletsgo button
+coolLetsGoButton = PROFILE.querySelector('.goToSplash');
+coolLetsGoButton.addEventListener('click', () => {
+  configureSplash();
+  gotoSplash();
+})
+
 // TODO:  When a user enters a room, start a process that queries for new chat messages every 0.1
 //        seconds. When the user leaves the room, cancel that process.
 //        (Hint: https://developer.mozilla.org/en-US/docs/Web/API/setInterval#return_value)
 
+async function startMessagePolling() {
+  message_interval = setInterval(async () => {
+  const chatbox = document.body.querySelector('.messages');
+  const curr_message = sessionStorage.getItem('curr_message');
+  const room_id = sessionStorage.getItem('room_id');
+  const api_key = localStorage.getItem('api-key');
+    // Get and fill chatbox
+    const chat_url = `/api/rooms/${room_id}`;
+    let chat_response = await fetch(chat_url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': api_key,
+      },
+    })
+    const messages = await chat_response.json();
+    console.log(messages);
+    if (messages['status'] == 'success'){
+      console.log(curr_message);
+      console.log(messages['message_dict'].length);
+      if (messages['message_dict'].length > curr_message) {
+        console.log('getting bingus');
+        for (let i = curr_message; i < messages['message_dict'].length; i++) {
+          let message = messages['message_dict'][i];
+          new_message = document.createElement('message');
+          new_author = document.createElement('author');
+          new_author.innerHTML = message['name'];
+          new_content = document.createElement('content');
+          new_content.innerHTML = message['body'];
+          new_message.appendChild(new_author);
+          new_message.appendChild(new_content);
+          chatbox.appendChild(new_message);
+        }
+      }
+      sessionStorage.setItem('curr_message', messages['message_dict'].length);
+    }
+  }, 500);
+}
+
+function stopMessagePolling() {
+  if (message_interval) {
+    clearInterval(message_interval);
+  }
+}
+
+window.addEventListener('popstate', function(event) {
+  console.log(event);
+  loadPageFromPath();
+})
+
 // On page load, show the appropriate page and hide the others
+configurePages();
+loadPageFromPath();
